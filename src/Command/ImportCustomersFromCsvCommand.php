6aad9de2f661b21f5e4b2cfe230f86e7fe1cb613
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-16 17:03:46 +0530

    add csv file

--- src/Command/ImportCustomersFromCsvCommand.php
+++ src/Command/ImportCustomersFromCsvCommand.php
@@ -1,54 +1,15 @@
 <?php
 
-namespace App\Command;
+use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\Folder;
 
-use Pimcore\Model\DataObject\Customer;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
-class ImportCustomersFromCsvCommand extends Command
-{
-    // IMPORTANT: must not be null
-    protected static $defaultName = 'app:import-customers';
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $filePath = \PIMCORE_PROJECT_ROOT . '/var/import/customers_500.csv';
-
-        if (!file_exists($filePath)) {
-            $output->writeln('CSV file not found: ' . $filePath);
-            return Command::FAILURE;
-        }
-
-        if (($handle = fopen($filePath, 'r')) === false) {
-            $output->writeln('Cannot open CSV file.');
-            return Command::FAILURE;
-        }
-
-        // adjust delimiter if needed
-        $headers = fgetcsv($handle, 0, ';');
-
-        while (($row = fgetcsv($handle, 0, ';')) !== false) {
-            $data = array_combine($headers, $row);
-
-            $customer = new Customer();
-            $customer->setKey(uniqid('cust_'));
-            $customer->setParentId(1); // TODO: correct folder ID
-            $customer->setPublished(true);
-
-            // TODO: adjust to your real fields
-            $customer->setFirstname($data['firstname'] ?? '');
-            $customer->setLastname($data['lastname'] ?? '');
-            $customer->setEmail($data['email'] ?? '');
-
-            $customer->save();
-        }
-
-        fclose($handle);
-
-        $output->writeln('Customers imported from CSV.');
-
-        return Command::SUCCESS;
-    }
+// Create or get /Customers folder
+$customersFolder = Folder::getByPath('/Customers');
+if (!$customersFolder) {
+    $customersFolder = new Folder();
+    $customersFolder->setKey('Customers');
+    $customersFolder->setParentId(1); // 1 = root of Data Objects
+    $customersFolder->save();
 }
+
+// You can now use $customersFolder->getId() as parentId for imported customers.
