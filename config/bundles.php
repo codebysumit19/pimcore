556a9cb2de30af49d33bd22f7eedd2b7e185fab2
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-16 21:49:23 +0530

    add csv file

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,5 @@
 <?php
 
-use Pimcore\Bundle\BundleGeneratorBundle\PimcoreBundleGeneratorBundle;
-
 return [
-    // other bundles ...
-
-    PimcoreBundleGeneratorBundle::class => ['dev' => true], // or ['all' => true] if you want it everywhere
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
 ];
