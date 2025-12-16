5e897ce50c2c2fc25f57aec29a8f3df47b7f2370
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-16 17:06:58 +0530

    add csv file

--- src/Command/ImportCustomersFromCsvCommand.php
+++ src/Command/ImportCustomersFromCsvCommand.php
@@ -1,15 +1,35 @@
 <?php
 
-use Pimcore\Model\DataObject;
+namespace App\Command;
+
+use Pimcore\Console\AbstractCommand;
 use Pimcore\Model\DataObject\Folder;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-// Create or get /Customers folder
-$customersFolder = Folder::getByPath('/Customers');
-if (!$customersFolder) {
-    $customersFolder = new Folder();
-    $customersFolder->setKey('Customers');
-    $customersFolder->setParentId(1); // 1 = root of Data Objects
-    $customersFolder->save();
+class CreateCustomersFolderCommand extends AbstractCommand
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName('app:create-customers-folder')
+            ->setDescription('Create /Customers folder in Data Objects');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $folder = Folder::getByPath('/Customers');
+        if (!$folder) {
+            $folder = new Folder();
+            $folder->setKey('Customers');
+            $folder->setParentId(1); // root of Data Objects
+            $folder->save();
+
+            $this->writeInfo('Created /Customers folder with ID: ' . $folder->getId());
+        } else {
+            $this->writeInfo('Folder /Customers already exists with ID: ' . $folder->getId());
+        }
+
+        return self::SUCCESS;
+    }
 }
-
-// You can now use $customersFolder->getId() as parentId for imported customers.
