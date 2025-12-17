d37ef617540fabdfb38fb36876af7b219b724e99
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-17 10:48:42 +0530

    ImportProspectsFromCsvCommand

--- src/Command/ImportFromProspectsCsvCommand.php
+++ src/Command/ImportFromProspectsCsvCommand.php
@@ -11,10 +11,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'app:import-prospects1',
+    name: 'app:import-prospects',
     description: 'Import prospects from CSV into /Prospects data object folder'
 )]
-class ImportProspectsFromCsvCommand1 extends AbstractCommand
+class ImportProspectsFromCsvCommand extends AbstractCommand
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
