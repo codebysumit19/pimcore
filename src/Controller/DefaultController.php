92db307c0f8da539d2a25edde5c67dafd2242175
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-17 17:10:38 +0530

    Default Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,23 +1,35 @@
 <?php
 
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 namespace App\Controller;
 
+use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends FrontendController
 {
-    /**
-     * This is called for simple pages that just need a template.
-     * By default Pimcore maps it to /templates/default/default.html.twig
-     * if you configure it as the documents.default_controller.
-     */
     public function defaultAction(Request $request): Response
     {
-        // Add any variables you want to pass to the template here.
-        return $this->render('default/default.html.twig', [
-            'foo' => 'bar',
-        ]);
+	    //p_r('Sumit123'); die;
+	    return $this->render('default/default.html.twig');
+    }
+
+    /**
+     * Forwards the request to admin login
+     */
+    public function loginAction(): Response
+    {
+        return $this->forward(LoginController::class.'::loginCheckAction');
     }
 }
