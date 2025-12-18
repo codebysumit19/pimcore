3fafbad8403aca2c54b673ac4f99641c086facc6
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-18 11:15:35 +0530

    SimulateCustomerEventsCommand

--- src/Command/SetupDataObjectFoldersCommand.php
+++ src/Command/SetupDataObjectFoldersCommand.php
@@ -17,7 +17,7 @@ class SetupDataObjectFoldersCommand extends AbstractCommand
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        foreach (['Customers', 'Dealers', 'Prospects'] as $name) {
+        foreach (['Customers', 'Dealers', 'Prospects', 'CustomerEvent'] as $name) {
             $folder = new DataObject\Folder();
             $folder->setKey(ElementService::getValidKey($name, 'object'));
             $folder->setParentId(1); // Home (root) in Data Objects
