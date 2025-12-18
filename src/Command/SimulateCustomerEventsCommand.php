d8afda3369d9499df1ea58b21e07d8057c17e238
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-18 11:34:35 +0530

    SimulateCustomerEventsCommand

--- src/Command/SimulateCustomerEventsCommand.php
+++ src/Command/SimulateCustomerEventsCommand.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\Customer;
-use Pimcore\Model\DataObject\CustomerEvent;
+use Pimcore\Model\DataObject\Events;              // <-- renamed
 use Pimcore\Model\DataObject\Service as ObjectService;
 use Pimcore\Model\Element\Service as ElementService;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -33,14 +33,13 @@ class SimulateCustomerEventsCommand extends AbstractCommand
 
         foreach ($list as $customer) {
             if (!$customer instanceof Customer || !$customer->getId()) {
-                // skip if not a persisted Customer object
                 continue;
             }
 
             for ($i = 0; $i < 3; $i++) {
                 $type = $types[array_rand($types)];
 
-                $event = new CustomerEvent();
+                $event = new Events();   // <-- use Events class
                 $event->setParent($eventsFolder);
                 $event->setKey(
                     ElementService::getValidKey(
@@ -50,16 +49,16 @@ class SimulateCustomerEventsCommand extends AbstractCommand
                 );
                 $event->setPublished(true);
 
-                // many-to-one relation field "customer"
+                // relation field (make sure field is still named "customer")
                 $event->setCustomer($customer);
 
-                // select field "eventType"
+                // select field
                 $event->setEventType($type);
 
-                // date & time field "eventTime" (Carbon)
+                // datetime field
                 $event->setEventTime(Carbon::now()->subDays(rand(0, 30)));
 
-                // text field "meta"
+                // text field
                 $event->setMeta('Mock event generated for demo');
 
                 $event->save();
